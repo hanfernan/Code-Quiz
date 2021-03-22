b9f76e19e095f0ee35cacf5100e4a8459f909c15
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-21 20:25:05 -0400

    updated the HTML and began pseudocoding

---
+++ script.js
@@ -0,0 +1,23 @@
+//these are some notes for my thought process
+//use .getElementById();
+
+//activity 5
+//.querySelectorAll to select all of a certain element 
+// or could use getElementById as well
+//use .setAttribute to change items. takes two arguments. first is the name of the attribute you want to modify and second is what you want to change.
+//activity 6
+//set styling for multiple elements w/ for loop.
+// for(var i = 0; i < varElementId.length; i++)) {
+//     varElementId.setAttribute(
+//         "style",
+//         "color: blue; font-size: 30px; font-weight: bold"
+//     );
+// }
+
+//activity 7
+//.createElement
+//remember the 3 step process!
+
+//for buttons, use data attributes for right or wrong and event listeners for the click
+//event bubbling to move through array for hiding questions. assign all questions to an array
+// use innerHTML to clear out an existing section 
