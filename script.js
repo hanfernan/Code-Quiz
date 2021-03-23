10852c3bec05bab9c0e0b89eb62721b29aa23bb1
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-23 09:58:19 -0400

    decided to define questions in JS instead of HTML

--- script.js
+++ script.js
@@ -1,47 +1,40 @@
 //define global variables
 var start = document.querySelector(".start");
-var firstQuest = document.querySelector(".first-question");
-var secondQuest = document.querySelector(".second-question");
-// var startButton = something;
+var startButton = document.querySelector(".start-button");
 var trueAnswer = document.querySelector(".true");
 var container = document.querySelector(".container");
+var questionContainer = document.querySelector("#question");
 
-//note to Hannah of tomorrow: you have been trying to make one section show up at a time using data attributes and event bubbling. Keep going! YOU GOT THIS!
-//this section is pulling from 04 Web APIs activity 20 on Data Attributes. I need to finish making it work for my purposes
-container.addEventListener("click", function(event) {
-    var element = event.target;
-    
-    if(element.matches(".question")) {
-        var state = element.getAttribute("data-state");
-        console.log(element.dataset)
+var questionBank = [
+  //define the questionBank variable by creating an array of objects
+  {
+    question: "What is my name?",
+    answers: [
+      { a: "Jude", correct: true },
+      { b: "bob", correct: false },
+      { c: "Allie", correct: false },
+    ],
+  },
+  {
+    question: "Who is our instructor?",
+    answers: [
+      { a: "Rebecca", correct: false },
+      { b: "Jonathan", correct: true },
+      { c: "Leo", correct: false },
+    ],
+  },
+  {},
+  {},
+];
 
-        if (state === "hidden") {
-            element.textContent = element.dataset.number;
-            element.dataset.state = "visible";
-        } else {
-            element.textContent= "";
-            element.setAttribute("data-state", "hidden")
-        }
+startButton.addEventListener("click", startGame);
 
-    }
-});
+function startGame() {
+  //hide instructions
+  start.classList.add("hidden");
 
-//functions
-//I will uses event bubbling to move through the questions
-function goToOne(event) {
-    event.stopPropagation();
-    event.currentTarget
+  questionContainer.classList.remove("hidden");
 
+  //show questions div
+  //start the timer
 }
-
-function goToTwo(event) {
-    event.stopPropagation();
-
-}
-//event listeners
-
-startButton.addEventListener("click", goToOne);
-trueAnswer.addEventListener("click", goToTwo);
-
-
-
