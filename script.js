b8e3d0c9454b950a14288480d4511df267d1d6e7
Hannah Fernandes-Martin <hfm11295@gmail.com>
2021-03-23 18:55:59 -0400

    added remaining questions

--- script.js
+++ script.js
@@ -13,29 +13,66 @@ var questionBank = [
     correctAnswer: "alerts",
   },
   {
-    question: "The condition in an if/else statement is enclosed within _______.",
-    answers: ["1. Quotes", "2. Curly Brackets", "3. Parentheses", "4. Square Brackets"],
+    question:
+      "The condition in an if/else statement is enclosed within _______.",
+    answers: [
+      "1. Quotes",
+      "2. Curly Brackets",
+      "3. Parentheses",
+      "4. Square Brackets",
+    ],
     correctAnswer: "Parentheses",
   },
-  {},
-  {},
+  {
+    question: "Arrays in JavaScript can be used to store ___.",
+    answers: [
+      "1. Numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+    correctAnswer: "",
+  },
+  {
+    question:
+      "4. String values must be enclosed within ___ when being assigned to variables.",
+    answers: ["	1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    correctAnswer: "",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console log",
+    ],
+    correctAnswer: "",
+  },
 ];
-
-startButton.addEventListener("click", startGame);
 
 function startGame() {
   //hide instructions
   start.classList.add("hidden");
   questionContainer.classList.remove("hidden");
+  //create the h1 element to hold the question
   var question = document.createElement("h1");
+  //put the question within that element
   question.textContent = questionBank[currentIndex].question;
+  //append the question to questionContainer
   questionContainer.appendChild(question);
-  //appending question to questionContainer
-  var answer = document.createElement("ol");
+  //create a div element to hold
+  var answer = document.createElement("div");
+  //create a for loop that will populate a button for each answerOption
   for (var i = 0; i < questionBank[currentIndex].answers.length; i++) {
-    var answerOption = document.createElement("li");
+    //define a variable to iterate through the answer options
+    var answerOption = document.createElement("button");
+    //assign text content from the questionBank to the answer buttons
     answerOption.textContent = questionBank[currentIndex].answers[i];
+    //add an event listener to run check function on click
     answerOption.addEventListener("click", check);
+    //append the answer options to the .answer div
     answer.appendChild(answerOption);
   }
   answers.appendChild(answer);
@@ -43,10 +80,17 @@ function startGame() {
 
 function check(event) {
   console.log(event.target.textContent);
-  if{
-    //tell computer when to stop
+  //show next question
+  //check if answer is right or wrong
+  if (
+    (questionBank[currentIndex].answers[i] =
+      questionBank[currentIndex].correctAnswer)
+  ) {
   }
-  if {
-    //check if answer is right or wrong
-  }
+  // if {
+  //   //tell computer when to stop
+
+  // }
 }
+
+startButton.addEventListener("click", startGame);
